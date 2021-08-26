314990b08e9419bc4b3d9ceb427f855debe30f32
ulrichloose <ulrich.loose@t-online.de>
2021-08-26 11:58:13 +0200

    some update

--- primitiveFTPd/src/org/primftpd/ui/StartServerAndExitActivity.java
+++ primitiveFTPd/src/org/primftpd/ui/StartServerAndExitActivity.java
@@ -3,24 +3,20 @@ package org.primftpd.ui;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-
+import org.primftpd.util.ServersRunningBean;
 import org.primftpd.util.ServicesStartStopUtil;
-
-class Toggle { static boolean running = false;   }
-
 public class StartServerAndExitActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Context context = getApplicationContext();
-        //ServicesStartStopUtil.startServers(context);
-        if (Toggle.running == false) {
+
+        ServersRunningBean serversRunningBean = ServicesStartStopUtil.checkServicesRunning(context);
+        if (!serversRunningBean.atLeastOneRunning()) {
             ServicesStartStopUtil.startServers(context);
-            Toggle.running = true;
         } else {
             ServicesStartStopUtil.stopServers(context);
-            Toggle.running = false;
         }
 
         // wait a short delay
